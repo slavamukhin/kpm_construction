2562ede962031610323cd9b06a53ab15554b4463
slavamukhin <via4eslav.mukhin@yandex.ru>
2022-06-14 10:14:14 +0300

    email was added

--- src/components/Phone/Phone.tsx
+++ src/components/Phone/Phone.tsx
@@ -1,9 +1,13 @@
 import { FC } from "react"
 import styled from "styled-components"
 
-const PnoneNumber = styled.a`
+const ContactLink = styled.a`
   text-decoration: none;
   color: #fff;
+  margin-right: 20px;
+  &:last-child {
+    margin-right: 0;
+  }
   &:hover {
     color: #e1e2e3;
   }
@@ -11,6 +15,7 @@ const PnoneNumber = styled.a`
 
 export const Phone: FC = () => {
   return <div>
-    <PnoneNumber href="tel:+16476884085">+1 647 688 40 85</PnoneNumber>
+    <ContactLink href="tel:+16476884085">+1 647 688 40 85</ContactLink>
+    <ContactLink href="mailto:kpm_construction@infor.com">kpm_construction@infor.com</ContactLink>
   </div>
 }
